6c8edbe5869c1775113a1b25ebb928ff5b70bf43
patrycjastarzec <patryc.sc@gmail.com>
2022-02-20 21:37:44 +0100

    PS-1 Added models and schemas

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 20,
+    required: true,
+  },
+  surname: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 20,
+    required: true,
+  },
+  username: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 10,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    minlength: 8,
+    maxlength: 20,
+    required: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    maxlength: 255,
+    unique: true,
+    index: true,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    maxlength: 9,
+    unique: true,
+    index: true,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
