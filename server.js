2541b29b1963aabba380c68c11ca4988fe8b4b46
Justyna Gładysz <jus.gladysz@gmail.com>
2022-02-21 00:32:33 +0100

    Refactor user authentication

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
 const {authenticateToken, refreshToken} = require("./controllers/auth.controller");
-const {generateAccessToken, generateRefreshToken, deleteToken} = require("./services/auth.service");
+const {deleteToken} = require("./services/auth.service");
+const {authUser, createUser, findAllUsers} = require("./services/users.service");
 const {findUserRecipes, findAllRecipes} = require("./services/recipe.service");
-
-//should be stored in database
-const users = []
-
-findAllUsers = async () => {
-    return users;
-}
 
 app.use(express.json())
 
@@ -20,15 +13,7 @@ app.get('/users', async (req, res) => {
     res.json(await findAllUsers())
 })
 
-app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = {name: req.body.name, password: hashedPassword}
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
+app.post('/users', createUser, (req, res) => {
 })
 
 app.get('/recipes', async (req, res) => {
@@ -45,21 +30,7 @@ app.post('/token', refreshToken, (req, res) => {
 app.delete('/logout', deleteToken, (req, res) => {
 })
 
-app.post('/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const [accessToken, refreshToken] = await Promise.all([generateAccessToken(user), generateRefreshToken(user)])
-            res.json({accessToken: accessToken, refreshToken: refreshToken})
-        } else {
-            res.send('Not Allowed')
-        }
-    } catch {
-        res.status(500).send()
-    }
+app.post('/login', authUser, async (req, res) => {
 })
 
 app.listen(process.env.PORT)
--- services/users.service.js
+++ services/users.service.js
@@ -1,5 +1,37 @@
+const bcrypt = require("bcrypt");
+const {generateAccessToken, generateRefreshToken} = require("./auth.service");
+
 //should be stored in database
 const users = []
+
+exports.authUser = async (req, res, next) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const [accessToken, refreshToken] = await Promise.all([generateAccessToken(user), generateRefreshToken(user)])
+            res.json({accessToken: accessToken, refreshToken: refreshToken})
+            next()
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+}
+
+exports.createUser = async (req, res, next) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {name: req.body.name, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+}
 
 exports.findAllUsers = async () => {
     return users;
