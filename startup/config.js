35518fa9c686b5a1243d347c64512b7c64d62aac
Tomasz <przadkatomasz.alt@gmail.com>
2022-02-23 00:24:27 +0100

    replace jwtPrivateKey validation with access_token_secret presence validation

--- startup/config.js
+++ startup/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const config = require("config");
 
 module.exports = function () {
-  if (!config.get("jwtPrivateKey")) {
-    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
+  if (!process.env.ACCESS_TOKEN_SECRET) {
+    throw new Error("FATAL ERROR: access_token is not defined.");
   }
 };
