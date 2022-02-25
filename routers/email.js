fa6dc095837e70d4b92277ea040182164683f8ab
justynas <staniszewskajustyn@gmail.com>
2022-02-25 20:52:28 +0100

    mail transporter

--- routers/email.js
+++ routers/email.js
@@ -1,11 +1,10 @@
 config("dotenv").config("nodemailer");
-//const express = require("express");
 const nodemailer = require("nodemailer");
-const Email = require("../models/emails");
-const router = express.Router();
+const mainMail = require("../models/emails");
+const path = require("path");
+const app = express();
 
-//const app = express();
-// const PORT = 3000;
+const PORT = 3000;
 
 // const transporter = nodemailer.createTransport({
 //   service: "gmail",
@@ -14,9 +13,37 @@ const router = express.Router();
 //   },
 //   auth: {
 //     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
+//     pass: process.env.PASS,
 //   },
 // });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+async function mainMail(name, email, message) {
+  const transporter = await nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+  const mailOption = {
+    from: process.env.USER,
+    to: process.env.EMAIL,
+    html: `You got a message from
+    Email : ${email}
+    Name: ${name}
+    Message: ${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    return Promise.resolve("Message Sent Successfully!");
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+}
 
 // transporter.sendMail(
 //   {
@@ -31,58 +58,10 @@ const router = express.Router();
 //   }
 // );
 
-// app.listen(PORT, () => {
-//   console.log("Server started on PORT", PORT);
-// });
-
-const path = require("path");
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
-async function mainMail(name, email, message) {
-  const transporter = await nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const mailOption = {
-    from: process.env.GMAIL_USER,
-    to: process.env.EMAIL,
-    html: `You got a message from 
-    Email : ${email}
-    Name: ${name}
-    Message: ${message}`,
-  };
-  try {
-    await transporter.sendMail(mailOption);
-    return Promise.resolve("Message Sent Successfully!");
-  } catch (error) {
-    console.log(error);
-    return Promise.reject(error);
-  }
-}
-
-app.get("/", (req, res) => {
-  res.render(index.html);
+app.listen(PORT, () => {
+  console.log("Server started on PORT", PORT);
 });
 
-app.get("/ContactForm", (req, res) => {
-  res.render(ContactForm.jsx);
+app.get("/", (req, res, next) => {
+  res.render("");
 });
-
-app.post("/ ContactForm", async (req, res, next) => {
-  const { yourname, youremail, yourmessage } = req.body;
-  try {
-    await mainMail(yourname, youremail, yourmessage);
-
-    res.send("Message Successfully Sent!");
-  } catch (error) {
-    res.send("Message Could not be Sent");
-  }
-});
-
-app.listen(3000, () => console.log("Server is running!"));
