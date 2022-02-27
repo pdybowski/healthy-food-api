46c925aa9c0a032b014108de653c236c9f48d015
Iwona Gorbacz <i.gorbacz@gmail.com>
2022-02-27 21:29:23 +0100

    fixed small problems

--- routers/passwordReset.js
+++ routers/passwordReset.js
@@ -3,12 +3,8 @@ const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const User = require("../models/user.model");
 
-const user = {
-  id: "hahahahaodji4893298",
-  email: "i.gorbacz@gmail.com",
-  password: "66898789",
-};
 
 router.get("/", (req, res, next) => {
   res.render("forgot-password");
@@ -17,19 +13,19 @@ router.get("/", (req, res, next) => {
 router.post("/", (req, res, next) => {
   const { email } = req.body;
   //make sure user exist in database
-  if (email !== user.email) {
+  if (email !== User.email) {
     res.send("User not registered");
     return;
   }
 
   //User exist and now create One time link valid 15 minutes
-  const secret = process.env.JWT_SECRET + user.password;
+  const secret = process.env.JWT_SECRET + User.password;
   const payload = {
-    email: user.email,
-    id: user.id,
+    email: User.email,
+    id: User.id,
   };
   const token = jwt.sign(payload, secret, { expiresIn: "15m" });
-  const link = `http://localhost:3000/reset-password/${user.id}/${token}`;
+  const link = `http://localhost:3000/reset-password/${User.id}/${token}`;
   console.log(link);
   res.send("Password reset link has been sent to your email...");
 });
@@ -37,15 +33,15 @@ router.post("/", (req, res, next) => {
 router.get("/reset-password/:id/:token", (req, res, next) => {
   const { id, token } = req.params;
   //check if this id exist in database
-  if (id !== user.id) {
+  if (id !== User.id) {
     res.send("Invalid id ...");
     return;
   }
   //we have a valid id, and we have a valid user with this id
-  const secret = process.env.JWT_SECRET + user.password;
+  const secret = process.env.JWT_SECRET + User.password;
   try {
     const payload = jwt.verify(token, secret);
-    res.render("reset-password", { email: user.email });
+    res.render("reset-password", { email: User.email });
   } catch (error) {
     console.log(error.message);
     res.send(error.message);
@@ -55,16 +51,16 @@ router.post("/reset-password/:id/:token", (req, res, next) => {
   const { id, token } = req.params;
   const { password, password2 } = req.body;
 
-  if (id !== user.id) {
+  if (id !== User.id) {
     res.send("Invalid id ...");
     return;
   }
-  const secret = process.env.JWT_SECRET + user.password;
+  const secret = process.env.JWT_SECRET + User.password;
   try {
     const payload = jwt.verify(token, secret);
     //we can simply find the user with the payload email and id an finally update new password
-    user.password = password;
-    res.send(user);
+    User.password = password;
+    res.send(User);
   } catch (error) {
     console.log(error.message);
     res.send(error.message);
