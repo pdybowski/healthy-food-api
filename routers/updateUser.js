82f1507c8a5c9c810a5c483c417f65eee5fd676e
Iwona Gorbacz <i.gorbacz@gmail.com>
2022-03-01 23:25:35 +0100

    latest fixes

--- routers/updateUser.js
+++ routers/updateUser.js
@@ -1,14 +1,7 @@
+const { updateUser } = require("../controllers/updateUser.controller");
+
 const router = require("express").Router();
 
-router.put("/update", async (req, res) => {
-  const updatedData = req.body;
-  await User.updateOne(
-    { email: req.body.email },
-    {
-      $set: req.body,
-    }
-  );
-  return res.status(200).json("updatedUser");
-});
+router.put("/update", updateUser);
 
 module.exports = router;
