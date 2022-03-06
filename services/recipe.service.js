dc9a7e8d4c98a1d2561b27a491b692a30978fe30
Tomasz <przadkatomasz.alt@gmail.com>
2022-03-06 23:25:14 +0100

    add recipe service

--- services/recipe.service.js
+++ services/recipe.service.js
@@ -1,16 +1,66 @@
 const Recipe = require('../models/recipe.model')
+const { NotFoundError } = require('../helpers/errorHandlers')
+const statusCodes = require('../helpers/httpStatusCode')
 
-const getSingleUserRecipe = async (recipe_id);
-const getManyUserRecipes = async();
-const createSingleRecipe = async()
-const updateSingleRecipe = async (recipe_id);
-const deleteSingleRecipe = async (recipe_id);
+
+
+const getOne = async (user_id, recipe_id) => {
+    const recipe = await Recipe.findOne({author: user_id, _id: recipe_id }).lean()
+    if (!recipe) {
+        throw new NotFoundError('No recipe found')
+    }
+
+    return recipe
+};
+const getMany = async(user_id) => {
+    const recipes = await Recipe.find({author: user_id})
+    if (!recipes) {
+        throw new NotFoundError('No recipes found')
+    }
+    return recipes
+}
+const createOne = async(req) => {
+    const recipe = new Recipe({
+        author: req.author,
+        title: req.title,
+        tags: req.tags,
+        time: req.time,
+        mealType: req.mealType,
+        ingredients: req.ingredients,
+        description: req.description,
+        recipe: req.recipe,
+        img: req.img,
+        peopleNumber: req.peopleNumber,
+        isFavourite: req.isFavourite,
+        favouriteCount: req.favouriteCount
+    })
+    await recipe.save()
+    return recipe
+};
+const updateOne = async(recipe_id, update_data) => {
+
+    const recipe = await Recipe.findById({_id: recipe_id})
+    if(!recipe)
+    {
+        throw  new NotFoundError('Recipe not found')
+    }
+    await Recipe.findByIdAndUpdate({_id: recipe_id}, update_data, {new: true})
+    return recipe
+};
+const deleteOne = async(recipe_id) => {
+    const recipe = await Recipe.findById({ _id: id });
+  if (!recipe) {
+    throw new NotFoundError("Meal plan doesn't exist");
+  }
+  await Recipe.deleteOne({ _id: id });
+  return true;
+};
 
 
 module.exports = {
-    getSingleUserRecipe,
-    getManyUserRecipes,
-    createSingleRecipe,
-    updateSingleRecipe,
-    deleteSingleRecipe
+    getOne,
+    getMany,
+    createOne,
+    updateOne,
+    deleteOne
 }
