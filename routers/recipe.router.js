9bc9c451ad76c2b63a86b9a75d3cd7515d5402ed
Tomasz <przadkatomasz.alt@gmail.com>
2022-03-13 23:34:16 +0100

    fix deleteRecipe and add createRecipe to routes

--- routers/recipe.router.js
+++ routers/recipe.router.js
@@ -6,7 +6,8 @@ const validateRecipe = require('../validation/recipe.validation')
 router.get('/recipes', RecipeController.getUserRecipes)
 router.get('/recipes/:id', RecipeController.getSingleRecipe)
 router.put('/recipes/:id', validate(validateRecipe), RecipeController.updateRecipe)
-router.post('/recipes/:id', validate(validateRecipe), RecipeController.deleteRecipe)
+router.post('/recipes/:id', RecipeController.deleteRecipe)
+router.post('/recipes/', validate(validateRecipe), RecipeController.createRecipe)
 
 
 module.exports = router
